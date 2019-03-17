4ce4a9e7ee8d6cf9fe80bb77d921ae00918be032
Moskalenko Sergey <oknelaksoms@gmail.com>
2019-03-17 18:28:50 +0200

    17.03.19

---
+++ Graph_Wizard/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Graph_Wizard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.додатиВершинуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.додатиРеброToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вибратиЕлементToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.додатиВершинуToolStripMenuItem,
+            this.додатиРеброToolStripMenuItem,
+            this.вибратиЕлементToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(705, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // додатиВершинуToolStripMenuItem
+            // 
+            this.додатиВершинуToolStripMenuItem.Name = "додатиВершинуToolStripMenuItem";
+            this.додатиВершинуToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.додатиВершинуToolStripMenuItem.Text = "Додати вершину";
+            this.додатиВершинуToolStripMenuItem.Click += new System.EventHandler(this.додатиВершинуToolStripMenuItem_Click);
+            // 
+            // додатиРеброToolStripMenuItem
+            // 
+            this.додатиРеброToolStripMenuItem.Name = "додатиРеброToolStripMenuItem";
+            this.додатиРеброToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.додатиРеброToolStripMenuItem.Text = "Додати ребро";
+            // 
+            // вибратиЕлементToolStripMenuItem
+            // 
+            this.вибратиЕлементToolStripMenuItem.Name = "вибратиЕлементToolStripMenuItem";
+            this.вибратиЕлементToolStripMenuItem.Size = new System.Drawing.Size(114, 20);
+            this.вибратиЕлементToolStripMenuItem.Text = "Вибрати елемент";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.hScrollBar1);
+            this.panel1.Controls.Add(this.vScrollBar1);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(432, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 520);
+            this.panel1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(248, 248);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.LargeChange = 1;
+            this.vScrollBar1.Location = new System.Drawing.Point(254, 3);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 248);
+            this.vScrollBar1.TabIndex = 1;
+            this.vScrollBar1.Visible = false;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 1;
+            this.hScrollBar1.Location = new System.Drawing.Point(3, 254);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(248, 17);
+            this.hScrollBar1.TabIndex = 2;
+            this.hScrollBar1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 548);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MinimumSize = new System.Drawing.Size(721, 587);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Побудова графа";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem додатиВершинуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem додатиРеброToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вибратиЕлементToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Graph_Wizard/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph_Wizard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int q = -1;
+        int[] x1, x2, y1, y2;
+        int id=0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "0";
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int abc = 1;
+            vertex[] v=new vertex[0];
+            edge[] ed = new edge[0];
+            if (q == 0)
+            {
+                //добавляем вершину
+                id++;
+                v = new vertex[id];
+                x1 = new int[id];
+                x2 = new int[id];
+                y1 = new int[id];
+                y2 = new int[id];
+                if (id > 1)
+                    for (int i = 0; i < id - 1; i++)
+                    {
+                        x1[i] = x2[i];
+                        y1[i] = y2[i];
+                    }
+                x1[id - 1] = e.X;
+                y1[id - 1] = e.Y;
+                x2[id - 1] = e.X;
+                y2[id - 1] = e.Y;
+                for (int i = 0; i < id; i++)
+                {
+                    v[i] = new vertex(x1[i], y1[i]);
+                }
+                
+            }
+            if (q == 1)
+            {
+                //добавляем грань
+                if (abc == 1)
+                {
+
+                }
+                if (abc == 2)
+                {
+
+                }
+                abc++;
+            }
+            if ((q == 0) || (q == 1 && abc == 3))
+                draw_graph(v, ed);
+        }
+
+        private void додатиВершинуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //timer1.Enabled = true;
+            q = 0;
+        }
+
+        class vertex
+        {
+            public int x, y;
+
+            public vertex(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        class edge
+        {
+            public int a, b;
+            public edge(int a,int b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+        }
+
+        void draw_graph(vertex[] V,edge[] E)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
